b27d46b430913d69e4fc1adf11fa7749180243fc
robSturcke <rob@rslabs.dev>
2022-07-30 20:57:23 -0400

    default static posts inside loader function

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,30 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+export const loader: LoaderFunction = () => {
+  return [{ title: 'first', body: 'My first post' }];
+};
+
+type Post = {
+  title: string;
+  body: string;
+};
+
 export default function Index() {
+  const posts = useLoaderData<Post[]>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {posts.map((post) => (
+          <li key={post.title}>
+            <div>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
